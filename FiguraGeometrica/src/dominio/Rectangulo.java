fdfd5b151ccdfd91908aeff5e5c4fa6cea7dc44e
ferbravo00 <fer.bp001@gmail.com>
2022-10-06 14:15:11 +0200

    cambios

---
+++ FiguraGeometrica/src/dominio/Rectangulo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dominio;
+
+/**
+ *
+ * @author Alumno Mañana
+ */
+public class Rectangulo extends FiguraGeometrica {
+    private int alto;
+    private int ancho;
+    
+    public void dibujar(){
+        
+    }
+}
